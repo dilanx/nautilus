48d5d2e23a4886608c12b12d3cc564bfe5e8a6f8
Dilan <dilan@dilanxd.com>
2023-05-03 16:14:34 -0500

    add net

---
+++ run-net.sh
@@ -0,0 +1,8 @@
+sudo qemu-system-x86_64 \
+	-smp 2 \
+	-m 2048 \
+	-vga std \
+	-serial stdio \
+	-cdrom nautilus.iso \
+	-device virtio-net,netdev=mynet0 \
+	-netdev user,id=mynet0,net=192.168.76.0/24,dhcpstart=192.168.76.9
