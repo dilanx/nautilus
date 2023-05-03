72e849b7c26d9423af69f3ed0add8b8e984a2c63
Dilan <dilan@dilanxd.com>
2023-05-03 17:30:09 -0500

    stuff

--- rn2.sh
+++ rn2.sh
@@ -5,4 +5,5 @@ qemu-system-x86_64 \
 	-serial stdio \
 	-cdrom nautilus.iso \
 	-device virtio-net,netdev=mynet0 \
-	-netdev user,id=mynet0,net=192.168.76.0/24,dhcpstart=192.168.76.9
+	-netdev user,id=mynet0,net=192.168.76.0/24,dhcpstart=192.168.76.9 \
+	-redir tcp:5555::7
